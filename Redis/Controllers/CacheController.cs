1b3fd4e72789383d8d48e4bcef45c74b1c0918d6
Mohamed orabi <mohamed.orabi97@outlook.com>
2021-10-02 16:49:48 +0200

    Add project files.

---
+++ Redis/Controllers/CacheController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Redis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly IDatabase _databasae;
+
+        public CacheController(IDatabase databasae)
+        {
+            _databasae = databasae;
+        }
+        // GET: api/<CacheController>
+        [HttpGet]
+        public string Get(string key)
+        {
+            return _databasae.StringGet(key);
+        }
+
+        // POST api/<CacheController>
+        [HttpPost]
+        public void Post([FromBody] KeyValuePair<string,string> keyValue)
+        {
+            _databasae.StringSet(keyValue.Key, keyValue.Value);
+        }
+
+    }
+}
